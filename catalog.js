5acc97f8946a56e8a5b5f808d44cd4f0af15364e
Yesypo <96166096+Yesypo@users.noreply.github.com>
2022-08-03 00:32:21 +0300

    Add files via upload

---
+++ catalog.js
@@ -0,0 +1,77 @@
+const catalog = [
+    {
+        id: 1,
+        name: "Apple iPhone 12 Pro Max 128 Gb Silver",
+        user: 'admin',
+        description: "Сріблястий флагман яблучної мобільної лінійки 2020 роки вміє вражати навіть досвідчених експертів! iPhone 12 Pro Max 128GB Silver володіє найкращим процесором і самим великим екраном серед інших моделей Apple, а можливості камери перевершують найсміливіші очікування.",
+        price: 42400,
+        image: 'https://www.istore.ua/upload/iblock/ae8/apple_iphone_12_pro_max_128gb_silver.jpg'
+    },
+    {
+        id: 2,
+        name: "Apple iPhone 12 Pro Max 128 Gb Silver",
+        user: 'admin',
+        description: "Сріблястий флагман яблучної мобільної лінійки 2020 роки вміє вражати навіть досвідчених експертів! iPhone 12 Pro Max 128GB Silver володіє найкращим процесором і самим великим екраном серед інших моделей Apple, а можливості камери перевершують найсміливіші очікування.",
+        price: 42200,
+        image: 'https://www.istore.ua/upload/iblock/ae8/apple_iphone_12_pro_max_128gb_silver.jpg'
+    },
+    {
+        id: 3,
+        name: "Apple iPhone 12 Pro Max 128 Gb Silver",
+        user: 'admin',
+        description: "Сріблястий флагман яблучної мобільної лінійки 2020 роки вміє вражати навіть досвідчених експертів! iPhone 12 Pro Max 128GB Silver володіє найкращим процесором і самим великим екраном серед інших моделей Apple, а можливості камери перевершують найсміливіші очікування.",
+        price: 40400,
+        image: 'https://www.istore.ua/upload/iblock/ae8/apple_iphone_12_pro_max_128gb_silver.jpg'
+    },
+    {
+        id: 4,
+        name: "Apple iPhone 12 Pro Max 128 Gb Silver",
+        user: 'admin',
+        description: "Сріблястий флагман яблучної мобільної лінійки 2020 роки вміє вражати навіть досвідчених експертів! iPhone 12 Pro Max 128GB Silver володіє найкращим процесором і самим великим екраном серед інших моделей Apple, а можливості камери перевершують найсміливіші очікування.",
+        price: 32400,
+        image: 'https://www.istore.ua/upload/iblock/ae8/apple_iphone_12_pro_max_128gb_silver.jpg'
+    },
+    {
+        id: 5,
+        name: "Apple iPhone 12 Pro Max 128 Gb Silver",
+        user: 'admin',
+        description: "Сріблястий флагман яблучної мобільної лінійки 2020 роки вміє вражати навіть досвідчених експертів! iPhone 12 Pro Max 128GB Silver володіє найкращим процесором і самим великим екраном серед інших моделей Apple, а можливості камери перевершують найсміливіші очікування.",
+        price: 400,
+        image: 'https://www.istore.ua/upload/iblock/ae8/apple_iphone_12_pro_max_128gb_silver.jpg'
+    },
+    {
+        id: 6,
+        name: "Apple iPhone 12 Pro Max 128 Gb Silver",
+        user: 'admin',
+        description: "Сріблястий флагман яблучної мобільної лінійки 2020 роки вміє вражати навіть досвідчених експертів! iPhone 12 Pro Max 128GB Silver володіє найкращим процесором і самим великим екраном серед інших моделей Apple, а можливості камери перевершують найсміливіші очікування.",
+        price: 43400,
+        image: 'https://www.istore.ua/upload/iblock/ae8/apple_iphone_12_pro_max_128gb_silver.jpg'
+    },
+    {
+        id: 7,
+        name: "Apple iPhone 12 Pro Max 128 Gb Silver",
+        user: 'admin',
+        description: "Сріблястий флагман яблучної мобільної лінійки 2020 роки вміє вражати навіть досвідчених експертів! iPhone 12 Pro Max 128GB Silver володіє найкращим процесором і самим великим екраном серед інших моделей Apple, а можливості камери перевершують найсміливіші очікування.",
+        price: 4400,
+        image: 'https://www.istore.ua/upload/iblock/ae8/apple_iphone_12_pro_max_128gb_silver.jpg'
+    },
+    {
+        id: 8,
+        name: "Apple iPhone 12 Pro Max 128 Gb Silver",
+        user: 'admin',
+        description: "Сріблястий флагман яблучної мобільної лінійки 2020 роки вміє вражати навіть досвідчених експертів! iPhone 12 Pro Max 128GB Silver володіє найкращим процесором і самим великим екраном серед інших моделей Apple, а можливості камери перевершують найсміливіші очікування.",
+        price: 42440,
+        image: 'https://www.istore.ua/upload/iblock/ae8/apple_iphone_12_pro_max_128gb_silver.jpg'
+    },
+    {
+        id: 9,
+        name: "Apple iPhone 12 Pro Max 128 Gb Silver",
+        user: 'admin',
+        description: "Сріблястий флагман яблучної мобільної лінійки 2020 роки вміє вражати навіть досвідчених експертів! iPhone 12 Pro Max 128GB Silver володіє найкращим процесором і самим великим екраном серед інших моделей Apple, а можливості камери перевершують найсміливіші очікування.",
+        price: 4220,
+        image: 'https://www.istore.ua/upload/iblock/ae8/apple_iphone_12_pro_max_128gb_silver.jpg'
+    },
+
+]
+
+module.exports = catalog
